2aca49c0f96afd36dbccc1ff5f3a44bb4f0742fb
Daria Kuznetsova <daryakuznetsova@bk.ru>
2023-02-27 20:42:18 +0300

    fourth homework

--- 4.objects/task.js
+++ 4.objects/task.js
@@ -1,19 +1,34 @@
 function Student(name, gender, age) {
-  
+    this.name = name;
+    this.gender = gender;
+    this.age = age;
+    this.marks = [];
 }
 
 Student.prototype.setSubject = function (subjectName) {
-  
-}
+  this.subject = subjectName;
+}  
 
 Student.prototype.addMarks = function (...marks) {
-  
+    if ('marks' in this) {
+        this.marks = [...marks];
+      }
 }
 
 Student.prototype.getAverage = function () {
+if (this.marks == 0) {
+  return 0;
+}
+  else {
+   let sum = 0;
+   let result = (this.marks.reduce((accamulator, item) => accamulator + item, sum)) / this.marks.length;
+    return result;
+  }
   
 }
 
 Student.prototype.exclude = function (reason) {
-  
+  this.excluded = reason;
+  delete this.marks;
+  delete this.subject;
 }
